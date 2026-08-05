454de1e0820d387af647ffb3a51b9e36bd064dcd
Rohit Kumar Mahto <rohitmahto062@gmail.com>
2026-08-05 11:24:29 +0530

    feat: add Cloudinary configuration and buffer upload utility for image storage

--- src/config/cloudinary.ts
+++ src/config/cloudinary.ts
@@ -1,6 +1,11 @@
 import { v2 as cloudinary } from 'cloudinary';
 import { env } from './env';
 
+
+// console.log("Cloud Name:", process.env.CLOUDINARY_CLOUD_NAME);
+// console.log("API Key:", process.env.CLOUDINARY_API_KEY);
+// console.log("API Secret Exists:", !!process.env.CLOUDINARY_API_SECRET);
+
 // Configure Cloudinary SDK
 if (env.CLOUDINARY_CLOUD_NAME && env.CLOUDINARY_API_KEY && env.CLOUDINARY_API_SECRET) {
   cloudinary.config({
